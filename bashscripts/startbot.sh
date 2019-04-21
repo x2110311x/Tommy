f334de9fb0ee6c9688282bb3d208fa7ec19591db
Alex Sweeney <x2110311x@gmail.com>
2019-04-21 19:51:12 -0400

    bashscripts

--- bashscripts/startbot.sh
+++ bashscripts/startbot.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-cd /bot/tommy
+cd /bot/tommy/
 source bin/activate
 cd bot
 python3 tommy.py
--- bashscripts/update.sh
+++ bashscripts/update.sh
@@ -3,5 +3,5 @@ cd /bots/tommy
 git fetch --all
 git reset --hard origin/master
 git pull origin master
-sudo chown root:root /bots/tommy/bashscripts/*
-sudo chmod 777 /bots/tommy/bashscripts/*
+sudo chown root:root /bots/tommy/bot/bashscripts/*
+sudo chmod 777 /bots/tommy/bot/bashscripts/*
