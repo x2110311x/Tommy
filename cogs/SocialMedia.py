a32536ff79be5a7f0b4f688152ebdce2fd9df872
Alex Sweeney <x2110311x@gmail.com>
2019-04-29 01:25:58 -0400

    Make sure bot doesn't react to itself for now

--- cogs/SocialMedia.py
+++ cogs/SocialMedia.py
@@ -13,33 +13,34 @@ class SocialMedia(commands.Cog, name="Social Media Feed"):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.webhook_id is not None:
-            if message.channel.id == 556298180057235483:
-                guild = self.bot.get_guild(config['server_ID'])
-                pingRole = guild.get_role(556306011443691541)
-                pingRole.edit(reason="Social Media Feed", mentionable=True)
-                await message.channel.send(f"{pingRole.mention}")
-                pingRole.edit(reason="Social Media Feed", mentionable=False)
+        if message.author != self.bot.user:
+            if message.webhook_id is not None:
+                if message.channel.id == 556298180057235483:
+                    guild = self.bot.get_guild(config['server_ID'])
+                    pingRole = guild.get_role(556306011443691541)
+                    pingRole.edit(reason="Social Media Feed", mentionable=True)
+                    await message.channel.send(f"{pingRole.mention}")
+                    pingRole.edit(reason="Social Media Feed", mentionable=False)
 
-            if message.channel.id == 556298669280985098:
-                guild = self.bot.get_guild(config['server_ID'])
-                pingRole = guild.get_role(556305967642574848)
-                pingRole.edit(reason="Social Media Feed", mentionable=True)
-                await message.channel.send(f"{pingRole.mention}")
-                pingRole.edit(reason="Social Media Feed", mentionable=False)
+                if message.channel.id == 556298669280985098:
+                    guild = self.bot.get_guild(config['server_ID'])
+                    pingRole = guild.get_role(556305967642574848)
+                    pingRole.edit(reason="Social Media Feed", mentionable=True)
+                    await message.channel.send(f"{pingRole.mention}")
+                    pingRole.edit(reason="Social Media Feed", mentionable=False)
 
-            if message.channel.id == 571332531224444948:
+                if message.channel.id == 571332531224444948:
+                    guild = self.bot.get_guild(config['server_ID'])
+                    pingRole = guild.get_role(572290246402768907)
+                    pingRole.edit(reason="Social Media Feed", mentionable=True)
+                    await message.channel.send(f"{pingRole.mention}")
+                    pingRole.edit(reason="Social Media Feed", mentionable=False)
+            elif message.channel.id == 556304268869763092:
                 guild = self.bot.get_guild(config['server_ID'])
-                pingRole = guild.get_role(572290246402768907)
+                pingRole = guild.get_role(556306039209984001)
                 pingRole.edit(reason="Social Media Feed", mentionable=True)
                 await message.channel.send(f"{pingRole.mention}")
                 pingRole.edit(reason="Social Media Feed", mentionable=False)
-        elif message.channel.id == 556304268869763092:
-            guild = self.bot.get_guild(config['server_ID'])
-            pingRole = guild.get_role(556306039209984001)
-            pingRole.edit(reason="Social Media Feed", mentionable=True)
-            await message.channel.send(f"{pingRole.mention}")
-            pingRole.edit(reason="Social Media Feed", mentionable=False)
 
 
 def setup(bot):
