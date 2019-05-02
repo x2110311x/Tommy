85152f52575721f1173514feb45202d20f867f53
Alex Sweeney <x2110311x@gmail.com>
2019-05-02 01:12:44 -0400

    Added report command

--- cogs/SuggestReport.py
+++ cogs/SuggestReport.py
@@ -16,6 +16,10 @@ with open(abspath(config['help_file']), 'r') as helpFile:
 helpInfo = helpInfo['SuggestionReport']
 
 
+class SaidCancelError(Exception):
+    pass
+
+
 class SaidNoError(Exception):
     pass
 
@@ -46,7 +50,7 @@ class SuggestReport(commands.Cog, name="Suggestion and Report Commands"):
 
                 suggestChan = self.bot.get_channel(config['suggestions_channel'])
                 await suggestChan.send(embed=embedSuggestion)
-                await author.send("Suggestion Subbmitted!")
+                await author.send("Suggestion Submitted!")
 
             except asyncio.TimeoutError:
                 await ctx.send("Timeout reached. Suggestion cancelled!")
@@ -86,7 +90,7 @@ class SuggestReport(commands.Cog, name="Suggestion and Report Commands"):
                     config['botSuggestHook'], data=jsdumps(submitData),
                     headers={'Content-Type': 'application/json'}
                 )
-                await author.send("Suggestion Subbmitted!")
+                await author.send("Suggestion Submitted!")
             except asyncio.TimeoutError:
                 await ctx.send("Timeout reached. Suggestion cancelled!")
         else:
@@ -102,6 +106,71 @@ class SuggestReport(commands.Cog, name="Suggestion and Report Commands"):
                 headers={'Content-Type': 'application/json'}
             )
             await ctx.send("Suggestion Submitted!")
+
+    @commands.command(brief=helpInfo['botsuggest']['brief'], usage=helpInfo['botsuggest']['usage'])
+    async def report(self, ctx):
+        author = ctx.message.author
+
+        def check2(m):
+            if m.author == author and m.channel == author.dm_channel:
+                if m.content.lower() == 'yes':
+                    return True
+                elif m.content.lower() == 'no':
+                    raise SaidNoError
+                else:
+                    return False
+            else:
+                return False
+
+        def check(m):
+            if m.author == author and m.channel == author.dm_channel:
+                if m.content.lower() == "cancel":
+                    raise SaidCancelError()
+                else:
+                    return True
+            else:
+                return False
+
+        await ctx.message.delete()
+        dmMsg = await ctx.send("Check your DMs")
+        await author.send("You can say cancel at any time to cancel the report")
+        await author.send("What is the name or ID of the user you want to report?")
+        await asyncio.sleep(2)
+        await dmMsg.delete()
+        try:
+            userMsg = await self.bot.wait_for('message', check=check, timeout=30)
+            await author.send("What channel did this happen in?")
+            chanMsg = await self.bot.wait_for('message', check=check, timeout=30)
+            await author.send("What is the reason for reporting this user?")
+            reasonMsg = await self.bot.wait_for('message', check=check, timeout=30)
+            await author.send("When did this happen?")
+            whenMsg = await self.bot.wait_for('message', check=check, timeout=30)
+            await author.send("Any other comments?")
+            commentsMsg = await self.bot.wait_for('message', check=check, timeout=30)
+
+            dateSubmitted = ctx.message.created_at.strftime("%m/%d/%Y, %H:%M:%S") + " GMT"
+            embedReport = discord.Embed(title="New report submission", colour=0x753543)
+            embedReport.set_author(name=ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
+            embedReport.add_field(name="Reported User", value=userMsg.content, inline=False)
+            embedReport.add_field(name="Happened in", value=chanMsg.content, inline=False)
+            embedReport.add_field(name="Time of incidence", value=whenMsg.content, inline=False)
+            embedReport.add_field(name="Reason for report", value=reasonMsg.content, inline=False)
+            embedReport.add_field(name="Other comments", value=commentsMsg.content, inline=False)
+            embedReport.set_footer(text=f"© x2110311x. Submitted at : {dateSubmitted}")
+
+            await author.send(embed=embedReport)
+            await author.send("Would you like to send this report?")
+            await self.bot.wait_for('message', check=check2, timeout=30)
+
+            reportChan = self.bot.get_channel(config['submitted_reports'])
+            await reportChan.send(embed=embedReport)
+            await author.send("Your report has been submitted")
+        except SaidCancelError:
+            await author.send("Okay, you can send one again at any time")
+        except SaidNoError:
+            await author.send("Okay, you can send one again at any time")
+        except asyncio.TimeoutError:
+            await author.send("Timeout reached. Report cancelled!")
 
 
 def setup(bot):
