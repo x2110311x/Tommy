38a1cf1f4601c1c610b3e2cd5cb488a0551fd0da
Alex Sweeney <x2110311x@gmail.com>
2019-05-05 14:51:36 -0400

    Added Stigmata role, Changed pingroles command

--- cogs/ShopandRoles.py
+++ cogs/ShopandRoles.py
@@ -80,8 +80,8 @@ class ShopandRoles(commands.Cog, name="Role Commands"):
         self.bot = bot
         self._last_member = None
 
-    @commands.command(brief=helpInfo['pingon']['brief'], usage=helpInfo['pingon']['usage'])
-    async def pingon(self, ctx):
+    @commands.command(brief=helpInfo['pingroles']['brief'], usage=helpInfo['pingroles']['usage'])
+    async def pingroles(self, ctx):
         user = ctx.message.author
 
         def check(m):
@@ -108,41 +108,8 @@ class ShopandRoles(commands.Cog, name="Role Commands"):
                     await self.bot.wait_for('message', check=check, timeout=30)
                     await ctx.message.author.add_roles(role)
                 except SaidNoError:
-                    pass
-            await user.send("Roles added! All done!")
-        except asyncio.TimeoutError:
-            await user.send("Timeout reached. Try again later!")
-
-    @commands.command(brief=helpInfo['pingoff']['brief'], usage=helpInfo['pingoff']['usage'])
-    async def pingoff(self, ctx):
-        user = ctx.message.author
-
-        def check(m):
-            if m.author == user and m.channel == user.dm_channel:
-                if m.content.lower() == 'yes':
-                    return True
-                elif m.content.lower() == 'no':
-                    raise SaidNoError
-                else:
-                    return False
-            else:
-                return False
-
-        await ctx.send("Check your DMs for more info")
-
-        roleList = []
-        for role in config['pingable_Roles']:
-            roleList.append(ctx.message.guild.get_role(role))
-
-        try:
-            for role in roleList:
-                await user.send(f"Do you want to remove the {role.name} role?")
-                try:
-                    await self.bot.wait_for('message', check=check, timeout=30)
                     await ctx.message.author.remove_roles(role)
-                except SaidNoError:
-                    pass
-            await user.send("Roles Removed! All done!")
+            await user.send("Roles Set! All done!")
         except asyncio.TimeoutError:
             await user.send("Timeout reached. Try again later!")
 
