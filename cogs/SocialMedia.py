93e507a90a5d0c23658d0a3bbe85d4f287874e73
Alex Sweeney <x2110311x@gmail.com>
2020-02-13 11:14:43 -0500

    Twitter Feed Updates
    This should fix double tweets and twitter bot dying?

--- cogs/SocialMedia.py
+++ cogs/SocialMedia.py
@@ -4,7 +4,7 @@ import twitter
 import time
 import yaml
 from datetime import datetime
-from discord.ext import commands
+from discord.ext import commands, tasks
 from os.path import abspath
 
 with open(abspath('./include/config.yml'), 'r') as configFile:
@@ -16,28 +16,6 @@ twitterAPI = twitter.Api(consumer_key=config['twitterAPIKey'],
                          access_token_secret=config['twitterAccessSecret'])
 
 
-async def twitter_check(self):
-    while self.bot.is_ready():
-        tweets = twitterAPI.GetUserTimeline(screen_name="grandson", exclude_replies=False, count=10)
-        for tweet in tweets:
-            if int(tweet.created_at_in_seconds) >= (int(time.time()) - 31):
-                embedTweet = discord.Embed(title="New Tweet from grandson", colour=0x753543)
-                postedBy = tweet.user.profile_image_url
-                embedTweet.set_thumbnail(url=postedBy)
-                embedTweet.add_field(name=f"http://twitter.com/grandson/status/{tweet.id}", value=tweet.text)
-                datePosted = datetime.utcfromtimestamp(int(tweet.created_at_in_seconds)).strftime("%m/%d/%Y, %H:%M:%S") + " GMT"
-                embedTweet.set_footer(text=datePosted, icon_url="http://x2110311x.me/twitter.png")
-                twitterChan = self.bot.get_channel(571332531224444948)
-                await twitterChan.send(embed=embedTweet)
-                guild = self.bot.get_guild(config['server_ID'])
-                pingRole = guild.get_role(572290246402768907)
-                await pingRole.edit(reason="Social Media Feed", mentionable=True)
-                await twitterChan.send(f"{pingRole.mention}")
-                await pingRole.edit(reason="Social Media Feed", mentionable=False)
-
-        await asyncio.sleep(30)
-
-
 async def youtube_check(self):
     while self.bot.is_ready():
         await asyncio.sleep(300)
@@ -45,8 +23,9 @@ async def youtube_check(self):
 
 class SocialMedia(commands.Cog, name="Social Media Feed"):
     def __init__(self, bot):
-        self.bot = bot
-        self._last_member = None
+        self.bot= bot
+        self._batch = []
+        self.twitter_check.start()
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -72,11 +51,43 @@ class SocialMedia(commands.Cog, name="Social Media Feed"):
                 await pingRole.edit(reason="Poll Ping", mentionable=True)
                 await message.channel.send(f"{pingRole.mention}")
                 await pingRole.edit(reason="Poll Ping", mentionable=False)
-
+        
     @commands.Cog.listener()
-    async def on_ready(self):
-        await twitter_check(self)
-
+    async def on_disconnect(self):
+        self.twitter_check.cancel()
+    
+    @tasks.loop(seconds=15.0, reconnect=True)
+    async def twitter_check(self):
+        try:
+            tweets = twitterAPI.GetUserTimeline(screen_name="grandson", exclude_replies=False, count=10)
+            for tweet in tweets:
+                if int(tweet.created_at_in_seconds) >= (int(time.time()) - 15):
+                    embedTweet = discord.Embed(title="New Tweet from grandson", colour=0x753543)
+                    postedBy = tweet.user.profile_image_url
+                    embedTweet.set_thumbnail(url=postedBy)
+                    embedTweet.add_field(name=f"http://twitter.com/grandson/status/{tweet.id}", value=tweet.text)
+                    datePosted = datetime.utcfromtimestamp(int(tweet.created_at_in_seconds)).strftime("%m/%d/%Y, %H:%M:%S") + " GMT"
+                    embedTweet.set_footer(text=datePosted, icon_url="http://x2110311x.me/twitter.png")
+                    twitterChan = self.bot.get_channel(571332531224444948)
+                    await twitterChan.send(embed=embedTweet)
+                    guild = self.bot.get_guild(config['server_ID'])
+                    pingRole = guild.get_role(572290246402768907)
+                    await pingRole.edit(reason="Social Media Feed", mentionable=True)
+                    await twitterChan.send(f"{pingRole.mention}")
+                    await pingRole.edit(reason="Social Media Feed", mentionable=False)
+        except:
+            chanTest = self.bot.get_channel(config['testing_Channel'])
+            await chanTest.send("Error handling Tweets")
+        finally:
+            guild = self.bot.get_guild(config['server_ID'])
+            pingRole = guild.get_role(572290246402768907)
+            await pingRole.edit(reason="Social Media Feed", mentionable=False)
+            
+    @twitter_check.before_loop
+    async def before_twitter_check(self):
+        await self.bot.wait_until_ready()
+        chanTest = self.bot.get_channel(config['testing_Channel'])
+        await chanTest.send("Twitter Feed Started")
 
 def setup(bot):
     bot.add_cog(SocialMedia(bot))
